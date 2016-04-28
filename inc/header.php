6abdfb2ba9868e9ffdf3610e520f3f097e46a73a
Nikki <dkn5678@gmail.com>
2016-04-28 10:19:32 -0400

    first upload of project so far

---
+++ inc/header.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<title>Quest Homebuilders | Welcome Home</title>
+<link rel="stylesheet" href="../css/foundation.min.css">
+</head>
+<body>
+
+<div class="top-bar">
+<div class="top-bar-left">
+  <ul class="menu">
+    <li class="menu-text"><a href="index.html">Quest Homebuilders</a></li>
+  </ul>
+</div>
+<div class="top-bar-right">
+  <ul class="dropdown menu" data-dropdown-menu>
+    <li><a href="#">House Types</a>
+      <ul class="menu">
+        <li><a href="../htp/Hamilton.php">Hamilton</a></li>
+        <li>Bristol 2</li>
+        <li>Danville</li>
+      </ul>
+    </li>
+    <li><a href="#">Available Homes</a>
+      <ul class="menu">
+        <li><a href="../ahp/1141BWaltersMill.php">1141B Walters Mill Rd</a></li>
+        <li>1621 Nuttal Ave</li>
+        <li>1805 Van Bibber</li>
+      </ul>
+    </li>
+    <li><a href="#">Contact Us</a></li>
+  </ul>
+</div>
+</div>
